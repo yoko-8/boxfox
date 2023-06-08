fbbbf1109d1f8a49258c244b8d648ce5f6a8b7fe
Yoko 8 <yo.ko.8.eight@gmail.com>
2023-06-07 17:24:27 -0700

    Completed 'about' section

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -17,7 +17,19 @@ export default function About({ switchPage }) {
               <img src={gitHubLogo} className='h-10 mb-1 transform active:scale-95 transition-transform' />
             </a>
           </div>
-        <p>Hi This is a neat project</p>
+        <div className='mt-8 px-2 py-1 bg-white rounded shadow'>
+          <p>
+            'boxfox' is a companion app for PostgreSQL databases.<br/>
+            <br/>
+            It allows users to remotely:<br/>
+            1. directly download tables as .csv files from their database, and<br/>
+            2. upload .csv files to their database as tables. If a table with the same name as an uploaded .csv file already exists, it will be replaced (NOTE: this feature is in beta - currently, all columns in the newly generated table will be of type TEXT).<br/>
+            <br/>
+            'boxfox' was created as a failsafe to be used in events when servers go down but users need to urgently access data from their PostgreSQL database. As such, 'boxfox' has been designed to connect to remote PostgreSQL database instances WITHOUT storing your credentials.<br/>
+            <br/>
+            'boxfox' is open source and was built with PostgreSQL, Express, React, Node.js, and TailwindCSS.<br/>
+          </p>
+        </div>
       </div>
     </div>
   )
